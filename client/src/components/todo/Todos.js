6913c4725060710101fe4d0216152ea7782f5ceb
Lisafeb <lisablaga@gmail.com>
2021-03-20 02:07:44 +0100

    Added animations for adding/removing todos

--- client/src/components/todo/Todos.js
+++ client/src/components/todo/Todos.js
@@ -1,6 +1,7 @@
 import React, { useContext, Fragment } from 'react'
 import TodoContext from "../../context/todo/TodoContext";
 import TodoItem from "./TodoItem";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Todos = () => {
     const todoContext = useContext(TodoContext);
@@ -9,9 +10,13 @@ const Todos = () => {
     console.log(todos)
     return (
         <Fragment>
-            {todos.map(todo => (
-                <TodoItem key={todo.id} todo={todo} />
-            ))}
+            <TransitionGroup>
+                {todos.map(todo => (
+                    <CSSTransition key={todo.id} timeout={500} classNames="item">
+                        <TodoItem key={todo.id} todo={todo} />
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </Fragment>
     )
 };
