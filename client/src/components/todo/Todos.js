bcdb487338efec58315491495fbd3f490f74ac93
Lisafeb <lisablaga@gmail.com>
2021-03-20 04:29:23 +0100

    Added support for dragging and dropping todo items WIP

--- client/src/components/todo/Todos.js
+++ client/src/components/todo/Todos.js
@@ -3,6 +3,7 @@ import TodoContext from "../../context/todo/TodoContext";
 import TodoItem from "./TodoItem";
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Spinner from '../layout/Spinner';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Todos = () => {
     const todoContext = useContext(TodoContext);
@@ -13,20 +14,50 @@ const Todos = () => {
         getTodos();
     }, []);
 
-    if(todos !== null && todos.length === 0 && !loading) {
+    function handleOnDragEnd(result) {
+        if (!result.destination) return;
+    
+        const items = Array.from(todos);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        console.log(items)
+        //updateTodos(items);
+      }
+
+    if (todos !== null && todos.length === 0 && !loading) {
         return <h4>Please add a contact</h4>
     }
     return (
         <Fragment>
             {todos !== null && !loading ? (
-            <TransitionGroup>
-                {todos.map(todo => (
-                    <CSSTransition key={todo._id} timeout={500} classNames="item">
-                        <TodoItem key={todo._id} todo={todo} />
-                    </CSSTransition>
-                ))}
-            </TransitionGroup>
-            ) : <Spinner/> }
+
+                <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <Droppable droppableId="todos">
+                        {(provided) => (
+                            <ul className="list-unstyled" {...provided.droppableProps} ref={provided.innerRef}>
+                                {todos.map(todo => (
+                                    <Draggable key={todo.title} draggableId={todo.title} index={todo.priority}>
+                                        {(provided) => (
+                                            <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                <TransitionGroup>
+                                                    <CSSTransition key={todo._id} timeout={500} classNames="item">
+
+                                                        <TodoItem key={todo._id} todo={todo} />
+                                                    </CSSTransition>
+                                                </TransitionGroup>
+                                            </li>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </ul>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+
+
+            ) : <Spinner />}
         </Fragment>
     )
 };
